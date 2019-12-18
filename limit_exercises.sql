89099c41900774c6450aa57fa6d75c31873e1fef
Nico Virgino <nicovirgino@gmail.com>
2019-12-18 16:32:00 -0600

    limit exercises complete

---
+++ limit_exercises.sql
@@ -0,0 +1,21 @@
+USE employees;
+
+SELECT DISTINCT title FROM titles;
+
+SELECT DISTINCT last_name
+FROM employees
+ORDER BY last_name DESC
+LIMIT 10;
+
+SELECT DISTINCT first_name, last_name, birth_date, hire_date
+FROM employees
+WHERE birth_date LIKE '%-12-25'
+ORDER BY birth_date, hire_date DESC
+LIMIT 5;
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date LIKE '%-12-25'
+  AND hire_date  BETWEEN '1990-01-01' AND '1999-12-31'
+ORDER BY birth_date, hire_date DESC
+LIMIT 5 OFFSET 45;
